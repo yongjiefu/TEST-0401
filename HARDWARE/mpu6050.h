e93b94fdc2f519a839f56bf07b635bd47e1d6038
fuyj <59347281+yongjiefu@users.noreply.github.com>
2023-04-02 10:33:35 +0800

    Delete HARDWARE directory

--- HARDWARE/mpu6050.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MPU6050_H
-#define __MPU6050_H	 
-
-#include "sys.h"
-
-void MPU6050_Init(void);
-unsigned int MPU6050_GetAngle(void);
-		 				    
-#endif
--- HARDWARE/timer.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TIMER_H
-#define __TIMER_H
-
-#include "stm32f10x.h"
-
-void TIM2_Init(void);
- 
-#endif
